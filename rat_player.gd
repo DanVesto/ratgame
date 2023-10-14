a49cd25f300b807efdd75320ee2b64bd16c98312
Aidan Vesto <dnvesto@gmail.com>
2023-10-14 13:01:28 -0500

    Tilemap and collisions

--- rat_player.gd
+++ rat_player.gd
@@ -4,7 +4,7 @@ func _physics_process(_delta):
 	# Get input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up"),
+		 Input.get_action_strength("down") - Input.get_action_strength("up"),
 	)
-	velocity = input_direction * move_speed
+	velocity = input_direction.normalized() * move_speed
 	move_and_slide()
