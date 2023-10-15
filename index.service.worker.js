442542fccc8f0b97cb593e6a876862c80cd70d6e
DanVesto <65134838+DanVesto@users.noreply.github.com>
2023-10-15 19:55:46 +0000

    Deploying to gh-pages from @ DanVesto/ratgame@af03e69b732414eec6848aa37217e56c55aee8f8 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1697327035|5360579";
+const CACHE_VERSION = "1697399733|5207007";
 const CACHE_PREFIX = "Rat Prototype 1-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
